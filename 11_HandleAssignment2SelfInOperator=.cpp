ae4b81ec3a28ce3cfda252a91accd2890995aefe
raven1989 <okuma0513@yahoo.com>
2017-06-29 20:19:55 +0800

    20170629@Jiuxianqiao how does stl swap

--- 11_HandleAssignment2SelfInOperator=.cpp
+++ 11_HandleAssignment2SelfInOperator=.cpp
@@ -73,27 +73,52 @@ class WidgetIII{
 public:
 	WidgetIII(BitMap& b);
 	WidgetIII& operator=(const WidgetIII& other){
-		WidgetIII replica(other);
-		swap(replica);
-    // std 提供的swap出现了段错误，再琢磨下
-    // std::swap(*this, replica);
+    if(!(this==&other)){
+      WidgetIII replica(other);
+      swap(replica);
+      // std 提供的swap出现了段错误，再琢磨下
+      // std::swap又调了operator=，循环调用了
+      // std::swap(*this, replica);
+    }
 		return *this;
 	}
   const string getBitMapName() const{
     return pb_->getName();
   }
-public:
+private:
 	void swap(WidgetIII& other){
 		// 交换*this和other的数据
     // 试试用memcpy来做
     WidgetIII tmp(*this);
-    memcpy(this, static_cast<const WidgetIII*>(&other), sizeof(*this));
-    memcpy(&other, static_cast<const WidgetIII*>(&tmp), sizeof(*this));
+    memcpy(this, reinterpret_cast<const void*>(&other), sizeof(*this));
+    memcpy(&other, reinterpret_cast<const void*>(&tmp), sizeof(*this));
+    // stl的swap：如果Alocator相同，就用赋值操作交换控制信息，其实和memcpy效果一样
+    // memcpy的效率显然更高
 	}
 public:
 	BitMap* pb_;
 };
 WidgetIII::WidgetIII(BitMap& b) : pb_(&b){
+}
+
+// 5. 上面在operator=中调用std::swap出错是因为std::swap调用了operator=，循环调用了
+class WidgetIV{
+public:
+  WidgetIV(BitMap& b);
+  WidgetIV& operator=(const WidgetIV& other){
+    cout << "operator= called by " << this->getBitMapName() << endl;
+    return *this;
+  }
+  void swap(WidgetIV& other){
+    ::std::swap(*this, other);
+  }
+  const string getBitMapName() const{
+    return pb_->getName();
+  }
+private:
+  BitMap* pb_;
+};
+WidgetIV::WidgetIV(BitMap& b) : pb_(&b){
 }
 
 int main(){
@@ -102,9 +127,18 @@ int main(){
   WidgetIII wl(bml);
   WidgetIII wr(bmr);
   cout << wl.getBitMapName() << " : " << wr.getBitMapName() << endl;
+  // std::swap
   swap(wl, wr);
   cout << wl.getBitMapName() << " : " << wr.getBitMapName() << endl;
   wl = wr;
   cout << wl.getBitMapName() << " : " << wr.getBitMapName() << endl;
+
+  BitMap bmivl("IV_left");
+  BitMap bmivr("IV_right");
+  WidgetIV ivl(bmivl);
+  WidgetIV ivr(bmivr);
+  cout << ivl.getBitMapName() << " : " << ivr.getBitMapName() << endl;
+  ivl.swap(ivr);
+  cout << ivl.getBitMapName() << " : " << ivr.getBitMapName() << endl;
 	return 0;
 }
